08cad12ef448ce31f123967b59d68c65fa78d6e0
Calvin <calvinjhfeng@gmail.com>
2016-09-11 16:09:00 -0700

    set up heroku instance for Redis store and configured it to AWS host

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,5 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+ENV["REDISTOGO_URL"] = "redis://h:pb56s1fjj1147takjkahe3r4n38@ec2-184-73-182-113.compute-1.amazonaws.com:12229"
--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,5 +1,10 @@
 require 'csv'
-$redis = Redis.new(:host => 'localhost', :port => 6379)
+require 'byebug'
+#$redis = Redis.new(:host => 'localhost', :port => 6379)
+host = "ec2-184-73-182-113.compute-1.amazonaws.com"
+port = 12229
+password = "pb56s1fjj1147takjkahe3r4n38"
+$redis = Redis.new(:host => host, :port => port, :password => password)
 
 $redis.flushdb
 csv_file = File.read('config/csv/ml-latest-small/movies.csv')
@@ -10,5 +15,4 @@ csv.each do |row|
   title = row["title"]
   movies[id] = {title: title}
 end
-
 $redis.set('movies', movies)
