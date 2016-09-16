ff2620e03f2b3a3ba2a2289b3ffa7bfc6be3fde8
Calvin <calvinjhfeng@gmail.com>
2016-09-16 16:40:18 -0700

    added config.cache_store = :memory_store to product config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -4,11 +4,13 @@ Rails.application.configure do
   # Code is not reloaded between requests.
   config.cache_classes = true
 
+  config.cache_store = :memory_store
   # Eager load code on boot. This eager loads most of Rails and
   # your application in memory, allowing both threaded web servers
   # and those relying on copy on write to perform better.
   # Rake tasks automatically ignore this option for performance.
   config.eager_load = true
+
 
   # Full error reports are disabled and caching is turned on.
   config.consider_all_requests_local       = false
