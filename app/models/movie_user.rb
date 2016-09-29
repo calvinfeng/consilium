dfecd886adfdbe3a77e6b2b802f29649a933bd47
Calvin <calvinjhfeng@gmail.com>
2016-09-28 21:01:52 -0700

    commented out unused models

--- app/models/movie_user.rb
+++ app/models/movie_user.rb
@@ -31,92 +31,92 @@
 #   91947 => 3.3
 # }
 
-class Movie
-  attr_reader :id, :features
-
-  def initialize(movie_id, features_of_movies)
-    @id = movie_id
-    @features = features_of_movies[movie_id]
-  end
-
-  def predicted_rating_for(user)
-    sum = 0
-    self.features.each_with_index do |feature, idx|
-      sum += feature * user.preferences[idx]
-    end
-    sum
-  end
-end
-
-class User
-  attr_reader :id, :ratings, :avg_rating, :preferences
-  def initialize(user_id, movie_ratings)
-    @id = user_id
-    @ratings = movie_ratings
-    @avg_rating = compute_avg_rating
-    @preferences = compute_preferences
-  end
-
-  def compute_avg_rating
-    avg_rating = 0
-    @ratings.each do |movie_id, rating|
-      avg_rating += rating
-    end
-    avg_rating = avg_rating/@ratings.length
-    return avg_rating
-  end
-
-  def cost
-    sq_error_sum = 0
-    l = 0.1
-    @ratings.each do |movie_id, rating|
-      movie = Movie.new(movie_id, FEATURES)
-      sq_error_sum += (movie.predicted_rating_for(self) - rating)**2
-    end
-    regularized_sum = 0
-    @preferences.each do |pref|
-      regularized_sum += pref**2
-    end
-    return sq_error_sum + l*regularized_sum
-  end
-
-  def compute_preferences
-    @preferences = Array.new(8) { rand }
-    alpha = 0.1
-    500.times do |i|
-      pref = @preferences.dup
-      cost = cost_hash
-      pref.each_index do |preference_idx|
-        pref[preference_idx] -= alpha * dF(preference_idx, cost)
-      end
-      @preferences = pref
-      puts "Iteration: #{i} cost: #{self.cost}"
-    end
-    @preferences
-  end
-
-  def cost_hash
-    hash = {}
-    @ratings.each do |movie_id, user_rating|
-      movie = Movie.new(movie_id, FEATURES)
-      predicted = movie.predicted_rating_for(self)
-      hash[movie_id] = predicted - user_rating
-    end
-    hash
-  end
-
-  def dF(preference_idx, cost)
-    step, lambda_for_pref = 0, 0.1
-    step = lambda_for_pref * @preferences[preference_idx] if preference_idx > 0
-
-    cost.each do |movie_id, cost|
-      movie = Movie.new(movie_id, FEATURES)
-      step += cost * movie.features[preference_idx]
-    end
-
-    step / @ratings.size
-  end
-end
+# class Movie
+#   attr_reader :id, :features
+#
+#   def initialize(movie_id, features_of_movies)
+#     @id = movie_id
+#     @features = features_of_movies[movie_id]
+#   end
+#
+#   def predicted_rating_for(user)
+#     sum = 0
+#     self.features.each_with_index do |feature, idx|
+#       sum += feature * user.preferences[idx]
+#     end
+#     sum
+#   end
+# end
+#
+# class User
+#   attr_reader :id, :ratings, :avg_rating, :preferences
+#   def initialize(user_id, movie_ratings)
+#     @id = user_id
+#     @ratings = movie_ratings
+#     @avg_rating = compute_avg_rating
+#     @preferences = compute_preferences
+#   end
+#
+#   def compute_avg_rating
+#     avg_rating = 0
+#     @ratings.each do |movie_id, rating|
+#       avg_rating += rating
+#     end
+#     avg_rating = avg_rating/@ratings.length
+#     return avg_rating
+#   end
+#
+#   def cost
+#     sq_error_sum = 0
+#     l = 0.1
+#     @ratings.each do |movie_id, rating|
+#       movie = Movie.new(movie_id, FEATURES)
+#       sq_error_sum += (movie.predicted_rating_for(self) - rating)**2
+#     end
+#     regularized_sum = 0
+#     @preferences.each do |pref|
+#       regularized_sum += pref**2
+#     end
+#     return sq_error_sum + l*regularized_sum
+#   end
+#
+#   def compute_preferences
+#     @preferences = Array.new(8) { rand }
+#     alpha = 0.1
+#     500.times do |i|
+#       pref = @preferences.dup
+#       cost = cost_hash
+#       pref.each_index do |preference_idx|
+#         pref[preference_idx] -= alpha * dF(preference_idx, cost)
+#       end
+#       @preferences = pref
+#       puts "Iteration: #{i} cost: #{self.cost}"
+#     end
+#     @preferences
+#   end
+#
+#   def cost_hash
+#     hash = {}
+#     @ratings.each do |movie_id, user_rating|
+#       movie = Movie.new(movie_id, FEATURES)
+#       predicted = movie.predicted_rating_for(self)
+#       hash[movie_id] = predicted - user_rating
+#     end
+#     hash
+#   end
+#
+#   def dF(preference_idx, cost)
+#     step, lambda_for_pref = 0, 0.1
+#     step = lambda_for_pref * @preferences[preference_idx] if preference_idx > 0
+#
+#     cost.each do |movie_id, cost|
+#       movie = Movie.new(movie_id, FEATURES)
+#       step += cost * movie.features[preference_idx]
+#     end
+#
+#     step / @ratings.size
+#   end
+# end
 
 # movies = Hash.new
 # movies[35836] = Movie.new(35836, FEATURES[35836])
