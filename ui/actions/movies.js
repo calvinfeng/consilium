a9c5c38685c0e861ce205c3b10bbc3500b0b977f
Calvin Feng <calvinjhfeng@gmail.com>
2017-02-19 23:18:02 -0800

    removed regressions component on server completely

--- ui/actions/movies.js
+++ ui/actions/movies.js
@@ -19,7 +19,7 @@ const trainingMoviesFetchSuccess = (data) => {
 const trainingMoviesFetchFail = (error) => {
     return {
         type: TRAINING_MOVIES_FETCH_FAIL,
-        error: error
+        error
     };
 };
 
@@ -34,3 +34,25 @@ export const trainingMoviesFetch = () => (dispatch) => {
             dispatch(trainingMoviesFetchFail(error));
         });
 };
+
+export const RECOMMENDED_MOVIES_FETCH_SUCCESS = 'RECOMMENDED_MOVIES_FETCH_SUCCESS';
+export const RECOMMENDED_MOVIES_FETCH_FAIL = 'RECOMMENDED_MOVIES_FETCH_FAIL';
+
+const recommendedMoviesFetchSuccess = (data) => {
+    return {
+        type: RECOMMENDED_MOVIES_FETCH_SUCCESS,
+        data
+    };
+}
+
+const recommendedMoviesFetchFail = (error) => {
+    return {
+        type: RECOMMENDED_MOVIES_FETCH_FAIL,
+        error
+    };
+}
+
+export const recommendedMoviesFetch = (movieRatings) => (dispatch) => {
+    return request
+        .get('api/recommender/')
+}
